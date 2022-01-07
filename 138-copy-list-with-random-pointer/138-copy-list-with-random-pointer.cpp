3300a748b6b9bfe07556c6b256a37661f9b1c154
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-01-08 01:01:05 +0530

    Time: 19 ms (6.52%), Space: 11.2 MB (87.59%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+     Node* dummyNode=new Node(-1);
+     Node* current=head;//old
+     Node* runner=dummyNode;//new
+        
+        unordered_map<Node*,Node*> memo;
+        
+        while(current!=NULL){
+            Node* newNode=new Node(current->val);
+            memo[current]= newNode;
+            runner->next=newNode;
+            runner=runner->next;
+            current=current->next;
+            
+        }
+        
+        current=head;
+        runner=dummyNode->next;
+        
+        while(current!=NULL){
+            
+            // if(current->random!=NULL)
+            runner->random=memo[current->random];
+            runner=runner->next;
+            current=current->next;
+        }
+        
+        return dummyNode->next;
+        
+         
+    }
+};
