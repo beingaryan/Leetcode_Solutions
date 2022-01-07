d870a2c6678a5872a4396b98a3eb56c409c6795e
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-01-07 23:59:55 +0530

    Time: 27 ms (6.86%), Space: 44.3 MB (5.00%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        return fun(prices,0, true, new HashMap<String, Integer>());
+    }
+    
+    public int fun(int [] prices, int cd, boolean canBuy,  HashMap<String, Integer> memo){
+        
+        
+        if(cd>=prices.length )return 0;
+        
+        
+        
+        String currkey=Integer.toString(cd)+'_'+Boolean.toString(canBuy)+'_';
+         if(memo.containsKey(currkey))return memo.get(currkey);
+        
+        int idle=fun(prices,cd+1,canBuy,memo);
+        
+       
+        if(canBuy==true)
+        {
+            int buy=-prices[cd]+fun(prices,cd+1,false,memo);
+             memo.put(currkey, Math.max(idle,buy));
+
+        }   
+        
+      
+        if(canBuy==false){
+           int sell=prices[cd]+fun(prices,cd+2,true,memo);
+             memo.put(currkey, Math.max(idle,sell));
+        }
+        
+        return memo.get(currkey);
+       
+    }
+}
