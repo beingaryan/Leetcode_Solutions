eb14e330966734e6852c84ab90fae43a73f98546
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-01-07 23:27:19 +0530

    Time: 638 ms (5.65%), Space: 534.1 MB (6.15%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        return fun(prices,0, true,1, new HashMap<String, Integer>());
+    }
+    
+    public int fun(int [] prices, int cd, boolean canBuy, int transaction, HashMap<String, Integer> memo){
+        
+        
+        if(cd>=prices.length || transaction==0)return 0;
+        
+        
+        
+        String currkey=Integer.toString(cd)+'_'+Boolean.toString(canBuy)+'_'+Integer.toString(transaction);
+         if(memo.containsKey(currkey))return memo.get(currkey);
+        
+        int idle=fun(prices,cd+1,canBuy,transaction,memo);
+        
+       
+        if(canBuy==true)
+        {
+            int buy=-prices[cd]+fun(prices,cd+1,false,transaction,memo);
+             memo.put(currkey, Math.max(idle,buy));
+
+        }   
+        
+      
+        if(canBuy==false){
+           int sell=prices[cd]+fun(prices,cd+1,true,transaction-1,memo);
+             memo.put(currkey, Math.max(idle,sell));
+        }
+        
+        return memo.get(currkey);
+       
+    }
+}
