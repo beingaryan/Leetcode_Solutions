cf9904877f42e46cdd34a0ff73e9b6ae14e0cfcf
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-01-09 12:34:36 +0530

    Time: 4 ms (69.93%), Space: 13.6 MB (17.60%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> answer;
+        vector<int>currentAnswer;
+        set<string> val;
+        sort(nums.begin(),nums.end());
+        noDuplicateSubset(nums, 0,answer,currentAnswer,val);
+        return answer;
+    }
+    
+    void noDuplicateSubset(vector<int>& nums,int idx,vector<vector<int>>&answer,vector<int>&currentAnswer,set<string>&val){
+        if(idx>=nums.size()){
+             string ans="";
+            for(int i=0;i<currentAnswer.size();i++){
+                int temp=currentAnswer[i];
+               ans+=((char)(temp+'0'));
+                ans+='_';
+            }
+            if(!val.count(ans)){
+                val.insert(ans);
+                answer.push_back(currentAnswer);
+                // ans.clear();
+            }
+            
+            return;
+            
+        }
+        
+        //1 2
+        //2 2
+        
+        //1 2 1 2 
+        
+//         2 1 2 1
+//         1 2 1 2 
+        
+//             1 2 1 2
+            
+//             1 2 
+//             2 2
+            
+//             1 2
+//             2 1
+            
+//             1 1
+//             2 2
+            
+//             1 1
+//             2 1
+            
+//             1 2
+//             2 1
+            
+//             2 1
+            //
+        
+        //1 2
+        //2 1
+        
+        currentAnswer.push_back(nums[idx]);
+        noDuplicateSubset(nums,idx+1,answer,currentAnswer,val);
+        currentAnswer.pop_back();
+        noDuplicateSubset(nums,idx+1,answer,currentAnswer,val);
+        return;
+    }
+};
