392d980308ef2120335fc7042cd89656de686bec
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-04 22:58:11 +0530

    Time: 16 ms (72.01%), Space: 10.6 MB (94.30%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,98 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>>ans;
-        vector<int>currentPath;
         int n=graph.size();
+        
         vector<bool>visited(n,false);
-        // currentPath.push_back(0);
-            // visited[n];
-        fun(graph,ans,0,visited,n,currentPath);
+        
+        vector<vector<int>>ans;
+        vector<int>path;
+        sourceToDestination(graph,visited,0,n-1,path,ans);
         return ans;
     }
     
-    
-    void fun(vector<vector<int>>& graph,vector<vector<int>> &ans, int vertex,vector<bool>&visited,int n, vector<int>&currentPath){
-        if(vertex==n-1){
-            currentPath.push_back(vertex);
-            ans.push_back(currentPath);
-             currentPath.pop_back();
-            return;
+    void sourceToDestination(vector<vector<int>>& graph,vector<bool>&visited,int source,int destination,vector<int>&path,vector<vector<int>>&ans)
+    {
+        if(source==destination){
+        path.push_back(source);
+        visited[source]=true;
+        // vector<int> vect2;
+        //     vect2=path;
+        ans.push_back(path);
+         path.pop_back();
+        visited[source]=false;
+        return;
         }
         
-        if(visited[vertex]==true)return;
         
-        visited[vertex]=true;
-        currentPath.push_back(vertex);
+        path.push_back(source);
+        visited[source]=true;
         
-        for(auto neighbours:graph[vertex])
-            fun(graph,ans,neighbours,visited,n,currentPath);
-        visited[vertex]=false;
-        currentPath.pop_back();
+        for(int i=0;i<graph[source].size();i++){
+            if(visited[graph[source][i]]==false)
+                sourceToDestination(graph,visited,graph[source][i],destination,path,ans);
+        }
         
+        path.pop_back();
+        visited[source]=false;
         return;
     }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// void sourceToTarget(vector<vector<int>>&graph,int currNode,vector<bool>&visited,int n,
-//                        vector<int>&currentPath,vector<vector<int>>&ans){
-//         if(currNode==n-1){
-//             currentPath.push_back(currNode);
-//             ans.push_back(currentPath);
-//             currentPath.pop_back();
-//             return;
-//         }
-//         if(visited[currNode]==true)
-//             return;
-        
-//         visited[currNode]=true;
-//         currentPath.push_back(currNode);
-        
-//         for(auto neighbours:graph[currNode]){
-//             sourceToTarget(graph,neighbours,visited,n,currentPath,ans);
-//         }
-//         visited[currNode]=false;
-//         currentPath.pop_back();
-        
-//     }
     
-//     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-//         vector<vector<int>>ans;
-//         int n=graph.size();
-//         vector<bool>visited(n);
-//         vector<int>currentPath;
-        
-//         sourceToTarget(graph,0,visited,n,currentPath,ans);
-//         return ans;
-//     }
+    
+    
+    
+    };
