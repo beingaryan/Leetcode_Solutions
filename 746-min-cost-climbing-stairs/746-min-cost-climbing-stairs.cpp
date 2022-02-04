1cb940db7c5c9142db32d14d95495cfedd9764ba
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-04 14:10:49 +0530

    Time: 24 ms (5.72%), Space: 15.5 MB (5.23%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+       unordered_map<int,int>mp;
+    int minCostClimbingStairs(vector<int>& cost) {
+          int a = mincost(cost,0);
+        int b = mincost(cost,1);
+        return min(a,b);
+    }
+    
+     int mincost(vector<int>&cost , int curr)
+    {
+      if(curr==cost.size())return 0;
+               if(curr>cost.size())return 1000;
+         
+        if(mp.find(curr)!=mp.end())
+            return mp[curr];
+         
+         int oneStep=cost[curr]+mincost(cost,curr+1);
+         int twoStep=cost[curr]+mincost(cost,curr+2);
+         
+         return mp[curr]=min(oneStep,twoStep);
+     }
+};
