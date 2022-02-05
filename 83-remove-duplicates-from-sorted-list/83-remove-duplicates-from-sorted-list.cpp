981f75a7f1d38d58008b04ff9027043c2c20b7db
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-05 15:32:10 +0530

    Time: 12 ms (65.82%), Space: 11.6 MB (79.41%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -11,21 +11,58 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-       ListNode* dummyNode=new ListNode(-101);
-        dummyNode->next=head;
+
+    ListNode* dummyNode=new ListNode(101);
+         // dummyNode->next=h
+    dummyNode->next=head;
+        
+        ListNode* curr=head;
         ListNode* prev=dummyNode;
-        ListNode* curr=dummyNode->next;
         
         while(curr!=NULL){
             if(prev->val==curr->val){
                 prev->next=curr->next;
                 curr=curr->next;
-            }else{
+            }
+            else{
                 prev=curr;
                 curr=curr->next;
             }
         }
         
         return dummyNode->next;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //        ListNode* dummyNode=new ListNode(-101);
+//         dummyNode->next=head;
+//         ListNode* prev=dummyNode;
+//         ListNode* curr=dummyNode->next;
+        
+//         while(curr!=NULL){
+//             if(prev->val==curr->val){
+//                 prev->next=curr->next;
+//                 curr=curr->next;
+//             }else{
+//                 prev=curr;
+//                 curr=curr->next;
+//             }
+//         }
+        
+//         return dummyNode->next;
     }
 };
