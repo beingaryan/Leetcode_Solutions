76401602ea57747fb0397eb73672cd25e45cae74
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-06 16:14:19 +0530

    Time: 96 ms (77.50%), Space: 30.1 MB (66.18%) - LeetHub

---
+++ 148-sort-list/148-sort-list.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+         mergeSort(&head);//,&firstHead,&secondHead);
+         return head;
+    }
+    
+     void mergeSort(ListNode** head){
+         ListNode* headNew=*head;
+         ListNode* firstHead; 
+         ListNode* secondHead;
+         
+         if(headNew==NULL||headNew->next==NULL)return;// NULL;
+         
+             divideInTwoList(headNew,&firstHead,&secondHead);
+             mergeSort(&firstHead);
+             mergeSort(&secondHead);
+             *head=merge(firstHead,secondHead);
+         // return *head;
+     }
+    
+    ListNode* merge(ListNode* firstHead, ListNode* secondHead)
+    {
+        ListNode* result;
+        
+        if(!firstHead)return secondHead;
+        if(!secondHead)return firstHead;
+        
+        if(firstHead->val<=secondHead->val){
+            result=firstHead;
+            result->next=merge(firstHead->next,secondHead);
+            
+        }else{
+            result=secondHead;
+            result->next=merge(firstHead,secondHead->next);
+        }
+        return result;
+        
+    }
+    
+    void divideInTwoList(ListNode*head, ListNode**firstHead, ListNode**secondHead){
+        ListNode* fast=head;
+        ListNode* slow=head;
+        //1 2 4 5 3 0
+        while(fast->next&&fast->next->next){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        
+        *secondHead=slow->next;
+        slow->next=NULL;
+        *firstHead=head;
+    }
+};
