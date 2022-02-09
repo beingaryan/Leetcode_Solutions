2946260edb9f7afad51d4eeefccaa70990174a89
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-09 23:43:53 +0530

    Time: 7 ms (13.72%), Space: 10.1 MB (7.67%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -14,9 +14,8 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p&&!q)return true;
         
-        if(!p)return false;
-        if(!q)return false;
-        if(p->val!=q->val)return false;
+        if(!p||!q||p->val!=q->val)return false;
+     
         
         return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
     }
