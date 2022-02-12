8aa8a3791758339ddda17eae4301007a66c15649
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-13 01:42:54 +0530

    Time: 266 ms (24.93%), Space: 46.3 MB (28.81%) - LeetHub

---
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        	return atMostKUniqueCharacters(nums,k)-atMostKUniqueCharacters(nums,k-1);
+    }
+    
+      int atMostKUniqueCharacters(vector<int>&s, int k){
+        unordered_map<int,int>mp;
+        int release=0;
+        int answer=0;
+        int distinct=0;
+      for(int i=0;i<s.size();i++){
+          
+          if(mp.find(s[i])!=mp.end())
+          {
+          mp[s[i]]++;       
+          }
+         
+     else {//(mp.find(s[i])==mp.end()){
+         mp[s[i]]=1;
+         distinct++;
+     }
+          // mp[s[i]]++;
+//       release<=i&&distinct
+          while(distinct>k){
+              mp[s[release]]--;
+            
+              if(mp[s[release]]==0){
+              mp.erase(s[release]);
+              distinct--;
+              }
+            
+            release++;
+              
+          }
+          
+          answer=answer+i-release+1;
+          
+      }
+      
+      return answer;
+    }
+};
