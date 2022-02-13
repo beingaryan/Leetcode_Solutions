51b34e1728e0c7ca70173f328521922a8e2f5173
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-13 22:13:24 +0530

    Time: 33 ms (29.69%), Space: 15.6 MB (12.60%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,118 +1,120 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& edges) {
- 
-
+    
         vector<int>ans;
-        vector<vector<int>>adj(n);
+        
+    vector<vector<int>>adj(n);
         
         for(int i=0;i<edges.size();i++){
             adj[edges[i][1]].push_back(edges[i][0]);
         }
-        
         unordered_set<int>visited;
         unordered_set<int>recStack;
         
         for(int i=0;i<n;i++){
-            if(visited.find(i)==visited.end()){
-                if(dfs(adj,i,visited,recStack,ans)){
+            if(visited.find(i)==visited.end())
+            {
+                if(dfs(i,adj,visited,recStack,ans)){
                     vector<int>v;
                     return v;
                 }
-
             }
         }
-        
         reverse(ans.begin(),ans.end());
-        
         return ans;
     }
     
-    bool dfs(vector<vector<int>>&adj,int currentVertex, unordered_set<int>&visited, unordered_set<int>&recStack,vector<int>&ans){
-        visited.insert(currentVertex);
-        recStack.insert(currentVertex);
+    bool dfs(int vertex, vector<vector<int>>&adj,unordered_set<int>&visited,unordered_set<int>&recStack,vector<int>&ans){
+        visited.insert(vertex);
+        recStack.insert(vertex);
         
-        for(auto neighbours:adj[currentVertex]){
-            if(visited.find(neighbours)==visited.end()){
-                if(dfs(adj,neighbours,visited,recStack,ans))
+        for(auto neighbours:adj[vertex]){
+            if(visited.find(neighbours)==visited.end())//not present
+            {
+                if(dfs(neighbours,adj,visited,recStack,ans))
                     return true;
-            }else if(recStack.find(neighbours)!=recStack.end()){
+            }
+            else if(recStack.find(neighbours)!=recStack.end())
+            {
+            
                 return true;
             }
         }
-        ans.push_back(currentVertex);
-        recStack.erase(currentVertex);
         
-        
+        ans.push_back(vertex);
+        recStack.erase(vertex);
         return false;
-        
-        
     }
-        
-        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // }
 //         vector<int>ans;
-        
-//     vector<vector<int>>adj(n);
+//         vector<vector<int>>adj(n);
         
 //         for(int i=0;i<edges.size();i++){
 //             adj[edges[i][1]].push_back(edges[i][0]);
 //         }
+        
 //         unordered_set<int>visited;
 //         unordered_set<int>recStack;
         
 //         for(int i=0;i<n;i++){
-//             if(visited.find(i)==visited.end())
-//             {
-//                 if(dfs(i,adj,visited,recStack,ans)){
+//             if(visited.find(i)==visited.end()){
+//                 if(dfs(adj,i,visited,recStack,ans)){
 //                     vector<int>v;
 //                     return v;
 //                 }
+
 //             }
 //         }
+        
 //         reverse(ans.begin(),ans.end());
+        
 //         return ans;
 //     }
     
-//     bool dfs(int vertex, vector<vector<int>>adj,unordered_set<int>&visited,unordered_set<int>&recStack,vector<int>&ans){
-//         visited.insert(vertex);
-//         recStack.insert(vertex);
+//     bool dfs(vector<vector<int>>&adj,int currentVertex, unordered_set<int>&visited, unordered_set<int>&recStack,vector<int>&ans){
+//         visited.insert(currentVertex);
+//         recStack.insert(currentVertex);
         
-//         for(auto neighbours:adj[vertex]){
-//             if(visited.find(neighbours)==visited.end())//not present
-//             {
-//                 if(dfs(neighbours,adj,visited,recStack,ans))
+//         for(auto neighbours:adj[currentVertex]){
+//             if(visited.find(neighbours)==visited.end()){
+//                 if(dfs(adj,neighbours,visited,recStack,ans))
 //                     return true;
-//             }
-//             else if(recStack.find(neighbours)!=recStack.end())
-//             {
-            
+//             }else if(recStack.find(neighbours)!=recStack.end()){
 //                 return true;
 //             }
 //         }
+//         ans.push_back(currentVertex);
+//         recStack.erase(currentVertex);
         
-//         ans.push_back(vertex);
-//         recStack.erase(vertex);
+        
 //         return false;
+        
+        
 //     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 };
