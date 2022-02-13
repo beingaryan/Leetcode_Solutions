1a64efc05a51533ab129c3b3b2f444404ea64e7e
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-13 22:11:31 +0530

    Time: 29 ms (36.01%), Space: 15.7 MB (11.96%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& edges) {
-        
+ 
+
         vector<int>ans;
         vector<vector<int>>adj(n);
         
@@ -47,4 +48,71 @@ public:
         
         
     }
+        
+        
+//         vector<int>ans;
+        
+//     vector<vector<int>>adj(n);
+        
+//         for(int i=0;i<edges.size();i++){
+//             adj[edges[i][1]].push_back(edges[i][0]);
+//         }
+//         unordered_set<int>visited;
+//         unordered_set<int>recStack;
+        
+//         for(int i=0;i<n;i++){
+//             if(visited.find(i)==visited.end())
+//             {
+//                 if(dfs(i,adj,visited,recStack,ans)){
+//                     vector<int>v;
+//                     return v;
+//                 }
+//             }
+//         }
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+//     }
+    
+//     bool dfs(int vertex, vector<vector<int>>adj,unordered_set<int>&visited,unordered_set<int>&recStack,vector<int>&ans){
+//         visited.insert(vertex);
+//         recStack.insert(vertex);
+        
+//         for(auto neighbours:adj[vertex]){
+//             if(visited.find(neighbours)==visited.end())//not present
+//             {
+//                 if(dfs(neighbours,adj,visited,recStack,ans))
+//                     return true;
+//             }
+//             else if(recStack.find(neighbours)!=recStack.end())
+//             {
+            
+//                 return true;
+//             }
+//         }
+        
+//         ans.push_back(vertex);
+//         recStack.erase(vertex);
+//         return false;
+//     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 };
