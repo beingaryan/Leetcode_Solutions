1210b81a567fe2423298929754e6b725f77702df
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-13 13:03:08 +0530

    Time: 149 ms (5.03%), Space: 28.5 MB (5.92%) - LeetHub

---
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+        unordered_map<int,int>mp;
+        int maxElement=60;
+        int n=time.size();
+        int total=0;
+        for(int i=0;i<n;i++){
+            
+            int curr=time[i];
+            int toCheck=(60-(curr%60));
+            
+            while(toCheck<=maxElement){
+                // if()
+                if(mp.find(toCheck)!=mp.end())
+                {
+                    total+=mp[toCheck];
+                }
+                toCheck+=60;
+            }
+            
+            mp[time[i]]++;
+            maxElement=max(maxElement,time[i]);
+        }
+        
+        return total;
+    }
+};
