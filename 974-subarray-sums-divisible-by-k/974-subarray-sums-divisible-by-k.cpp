774dff066c5bf4430dc5097195b699b89ba1fa93
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-13 13:11:42 +0530

    Time: 159 ms (5.02%), Space: 32.1 MB (10.26%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -10,14 +10,11 @@ public:
         for(int i=0;i<nums.size();i++){
           prefixSum+=nums[i];
             int rem=((prefixSum%k)+k)%k;
-            if(mp.find(rem)==mp.end()){
-                mp[rem]=1;
-            }
-            else{
+            
                 ans=ans+mp[rem];
                 mp[rem]++;
             }
-        }
+        
         
         return ans;
         
