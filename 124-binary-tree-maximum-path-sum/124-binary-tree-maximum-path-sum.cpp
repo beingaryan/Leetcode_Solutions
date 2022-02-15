58edfed82af67b46f8e47c7c3a813935d25c74f3
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-16 01:59:19 +0530

    Time: 54 ms (15.27%), Space: 28.1 MB (37.33%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -17,6 +17,23 @@ public:
             maxVal(root,maxV);
         return maxV; 
     }
+//     What does a path look like according to this description?
+
+//  a path is always expressed as left nodes(if any) -> root -> right nodes(if any).
+// We can see that there are four cases for this node's sum:
+
+// Include both the left and the right nodes' results (i.e the maximum sum which passes through those respective children).
+// Include only the left result.
+// Include only the right result.
+// Include only the root node, i.e include none of the children in the result. (this is also the base case for a leaf node).
+    
+    
+// This way, we calculate the maximum of all four of these and update the global maximum, if required. 
+// But to return we have three choices, not four. 
+// The case where we include both the left and right subtrees cannot be returned to upper levels,
+// since a path cannot go from the right subtree to the parent of the current root. Hence for returning, 
+// we only consider the cases 2, 3 and 4.
+    
     
     int maxVal(TreeNode* root, int &maxV){
         if(!root)return 0;
@@ -24,7 +41,8 @@ public:
         int left=maxVal(root->left,maxV);
         int right=maxVal(root->right,maxV);
         
-        int currSum=max(root->val,max(root->val+left,root->val+right));
+        int currSum=max(root->val,max(root->val+left,root->val+right));//
+        
         maxV=max(maxV,max(currSum,root->val+left+right));
         
         
