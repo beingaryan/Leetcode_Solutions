c3fdb49ab1a9d1a41a286e552daadf6ea31df1c7
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-17 17:44:51 +0530

    Added solution - LeetHub

---
+++ Kth largest element in a stream - GFG/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+  public:
+    vector<int> kthLargest(int k, int arr[], int n) {
+        // priority queue<int
+        priority_queue<int,vector<int>, greater<int> >pq;
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            if(i<k)
+            pq.push(arr[i]);
+            
+            if(i==k-1){
+            v.push_back(pq.top());
+            continue;
+            }
+            
+            
+            if(i<k-1)
+            {
+            v.push_back(-1);
+            continue;
+            }
+            
+           if(arr[i]>pq.top()){
+             
+               pq.pop();
+               pq.push(arr[i]);
+                 v.push_back(pq.top());
+           }
+           else{
+               v.push_back(pq.top());
+           }
+            
+            
+        }
+        return v;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k,n;
+        cin>>k>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        vector<int> v = ob.kthLargest(k,arr,n);
+        for(int i : v)
+            cout<<i<<" ";
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
