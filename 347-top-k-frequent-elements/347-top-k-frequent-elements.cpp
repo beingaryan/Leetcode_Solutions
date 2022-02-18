65fa095e848402f0c42767fd97c8fd2e6a6e21ef
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-18 16:11:32 +0530

    Time: 19 ms (63.06%), Space: 13.5 MB (97.72%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -7,20 +7,22 @@ public:
         for(auto num:nums)
             mp[num]++;
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>minHeap;
+        vector<int>ans;
         
         for(auto ele:mp){
             minHeap.push({ele.second,ele.first});//freq,element
             if(minHeap.size()>k)
                 minHeap.pop();
-            
+           
             
         }
-        vector<int>ans;
-        while(!minHeap.empty())
+        
+          while(!minHeap.empty())
         {
            ans.push_back(minHeap.top().second);
             minHeap.pop();
         }
+       
         return ans;
         
     }
