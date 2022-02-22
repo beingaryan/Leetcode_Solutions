cc486de2055cfef8fe0e720787fba39aa18d82e8
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-22 21:22:37 +0530

    Time: 228 ms (30.93%), Space: 39.8 MB (88.43%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<pair<int,int>>adj[n+1];
+        
+        for(int i=0;i<times.size();i++){
+            adj[times[i][0]].push_back({times[i][1],times[i][2]});//source,target,weight
+        }
+        
+         bool visit[n+1];
+        memset(visit, false, sizeof(visit));
+        vector<int>distance(n+1,INT_MAX);
+        
+        distance[k]=0;
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        
+        pq.push({0,k});//dist,src
+          
+        while(!pq.empty()){
+            
+            int oldV=pq.top().second;
+            pq.pop();
+        for(auto neighbours:adj[oldV]){
+            int newV=neighbours.first;
+            int oldToNewDist=neighbours.second;
+            
+            if(visit[newV]==false&&distance[newV]>distance[oldV]+oldToNewDist){
+                distance[newV]=distance[oldV]+oldToNewDist;
+                pq.push({distance[newV],newV});
+            }
+        }
+           
+    
+    } 
+        int ans=-1;
+            for(int i=1;i<n+1;i++){
+                ans=max(ans,distance[i]);
+            }
+        if(ans==INT_MAX)return -1;
+        return ans;   
+    }
+};
