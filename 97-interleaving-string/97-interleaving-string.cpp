e1f489f7282b6d6480fc206c59a7b0074ddf63e8
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-22 15:46:18 +0530

    Time: 4 ms (71.93%), Space: 7.1 MB (45.59%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -15,10 +15,10 @@ public:
     bool solve(string &s1,string &s2, string &s3, int i, int j, int k, int s1len,int s2len,int s3len, vector<vector<int>> &dp){
         if(i==s1len&&j==s2len)
             return true;
-        if(dp[i][j]!=-1)return dp[i][j];
+       
         if(i>s1len||j>s2len||(s1[i]!=s3[k])&&s2[j]!=s3[k])
             return false;
-        
+         if(dp[i][j]!=-1)return dp[i][j];
         return dp[i][j]=((s1[i]==s3[k])&&solve(s1,s2,s3,i+1,j,k+1,s1len,s2len,s3len,dp))||((s2[j]==s3[k])&&(solve(s1,s2,s3,i,j+1,k+1,s1len,s2len,s3len,dp)));
         
     }
