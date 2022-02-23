525b22cd19a1fb8c390c16ab16c161c5c932fa1e
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-23 12:07:07 +0530

    Time: 86 ms (5.15%), Space: 13.2 MB (8.16%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -6,17 +6,17 @@ public:
         
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
         
-        return LCS(dp,m-1,n-1,text1,text2);
+        return LCS(dp,m,n,text1,text2);
     }
     
     int LCS(vector<vector<int>>&dp,int m,int n,string &text1,string &text2){
-        if(m<0||n<0)
+        if(m<=0||n<=0)
             return 0;
         
         if(dp[m][n]!=-1)
             return dp[m][n];
         
-        if(text1[m]==text2[n])
+        if(text1[m-1]==text2[n-1])
             dp[m][n]=1+LCS(dp,m-1,n-1,text1,text2);
     else{
         dp[m][n]=max(LCS(dp,m,n-1,text1,text2),LCS(dp,m-1,n,text1,text2));
