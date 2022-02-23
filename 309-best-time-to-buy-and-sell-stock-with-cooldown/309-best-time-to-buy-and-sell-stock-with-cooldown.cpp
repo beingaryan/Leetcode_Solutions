a9f5b303a4d43cc980aab96d7aa24b80469c66b0
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-23 11:14:40 +0530

    Time: 4 ms (75.36%), Space: 12.1 MB (14.85%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+     vector<vector<int>>dp(prices.size()+1,vector<int>(2,-1));
+        
+        //prices.size() canBuy kTransaction
+        int currDay=0;
+        int canBuy=1;
+        // int kTrans=1;
+        return maxEarn(dp,prices,currDay, canBuy, prices.size());
+        
+    }
+    
+    int maxEarn(vector<vector<int>>&dp,vector<int>& prices, int currDay, int canBuy, int n ){
+        
+       if(currDay>=n)return 0;
+        
+        // if(kTrans<=0)return 0;
+        
+        if(dp[currDay][canBuy]!=-1)
+            return dp[currDay][canBuy];
+        
+        if(canBuy==1){
+            int buy=-prices[currDay]+maxEarn(dp,prices,currDay+1,0,n);
+            int idle=maxEarn(dp,prices,currDay+1,canBuy,n);
+            
+            dp[currDay][canBuy]=max(buy,idle);
+        }else if(canBuy==0){
+            
+             int sell=prices[currDay]+maxEarn(dp,prices,currDay+2,1,n);
+            int idle=maxEarn(dp,prices,currDay+1,canBuy,n);
+              dp[currDay][canBuy]=max(sell,idle);
+        }
+       return  dp[currDay][canBuy];
+    }
+    
+};
