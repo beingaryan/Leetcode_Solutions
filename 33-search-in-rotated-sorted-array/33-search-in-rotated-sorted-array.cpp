5c724f1299a434c39c05c5833e7905d984962312
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-24 17:10:52 +0530

    Time: 4 ms (74.23%), Space: 11.1 MB (76.20%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,40 +1,72 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+       //5 6 7 1 2
+      //   //7 8 9 1 2 3 4 5
+      // 5 6 7 8 9 1
+      //       c=8
+      //       t=9
         int low=0;
         int high=nums.size()-1;
-        // int comparator=0;
-        int comparator=0;
+        
+        int comparator=0;//midElement
         int pivot=nums[0];
         while(low<=high){
             int mid=low+(high-low)/2;
             
-            if(((target>=pivot)&&(nums[mid]>=pivot))||((target<pivot)&&(nums[mid]<pivot))){
+            if((nums[mid]>=pivot&&target>=pivot)||(nums[mid]<pivot&&target<pivot)){
                 comparator=nums[mid];
             }
-            
             else{
                 if(target>=pivot)
                     comparator=INT_MAX;
-                else
+                else if(target<=pivot)
                     comparator=INT_MIN;
-                
             }
             
-            if(target==comparator)return mid;
-            
-            if(target>comparator){
-                low=mid+1;
-            }
-            else{
+            if(target==comparator)
+                return mid;
+            // 5 6 7 1 2
+            if(target<comparator)
                 high=mid-1;
-            }
-                      
+            else
+            low=mid+1;
+                
         }
-        
         return -1;
     }
 };
+
+//  int low=0;
+//         int high=nums.size()-1;
+//         int comparator=0;
+//         int pivot=nums[0];
+//         while(low<=high){
+//             int mid=low+(high-low)/2;
+            
+//             if(((target>=pivot)&&(nums[mid]>=pivot))||((target<pivot)&&(nums[mid]<pivot))){
+//                 comparator=nums[mid];
+//             }
+            
+//             else{
+//                 if(target>=pivot)
+//                     comparator=INT_MAX;
+//                 else
+//                     comparator=INT_MIN;
+                
+//             }
+            
+//             if(target==comparator)return mid;
+            
+//             if(target>comparator){
+//                 low=mid+1;
+//             }
+//             else{
+//                 high=mid-1;
+//             }
+                      
+//         }
+        
 // Source: https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/154836/The-INF-and-INF-method-but-with-a-better-explanation-for-dummies-like-me
 
 // class Solution {
