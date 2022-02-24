f9532f137ecec318bd07b0b5653713d9b1760685
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-24 23:54:53 +0530

    Time: 43 ms (34.58%), Space: 13.5 MB (57.41%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,35 +1,63 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
+    int n=graph.size();
         
-        vector<int>set(n,-1);
-        // vector<int>visited(n,0);
+        vector<int>visited(n+1,-1);
         
-        for(int i=0;i<n;i++)
-        {
-            if(set[i]==-1)//not yet visited
-            {
-                if((checkEvenLength(set,i,graph,0))==false){
+        for(int i=0;i<n;i++){
+            if(visited[i]==-1){
+                if((checkEvenNodes(i,visited,0,graph)==false))
                     return false;
-                }
             }
         }
-        
-        return true;
+    return true;
     }
     
-    bool checkEvenLength(vector<int>&set,int vertex,vector<vector<int>>& graph,int setIndex ){
-        if(set[vertex]!=-1)
-            return setIndex==set[vertex];
+    
+    bool checkEvenNodes(int vertex,vector<int>&visited,int colourIndex,vector<vector<int>>&graph){
+        if(visited[vertex]!=-1)
+            return visited[vertex]==colourIndex;
         
-        set[vertex]=setIndex;
+        visited[vertex]=colourIndex;
         
-        for(auto neighbours:graph[vertex]){
-            if(checkEvenLength(set,neighbours,graph,1-setIndex)==false)
+        for(auto neigh:graph[vertex]){
+            if(checkEvenNodes(neigh,visited,1-colourIndex,graph)==false)
                 return false;
         }
-        
         return true;
     }
+//     int n=graph.size();
+        
+//         vector<int>set(n,-1);
+//         // vector<int>visited(n,0);
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             if(set[i]==-1)//not yet visited
+//             {
+//                 if((checkEvenLength(set,i,graph,0))==false){
+//                     return false;
+//                 }
+//             }
+//         }
+        
+//         return true;
+//     }
+    
+//     bool checkEvenLength(vector<int>&set,int vertex,vector<vector<int>>& graph,int setIndex)
+//     {
+//         if(set[vertex]!=-1)
+//             return setIndex==set[vertex];
+        
+//         set[vertex]=setIndex;
+        
+//         for(auto neighbours:graph[vertex])
+//         {
+//             if(checkEvenLength(set,neighbours,graph,1-setIndex)==false)
+//                 return false;
+//         }
+        
+//         return true;
+//     }
 };
