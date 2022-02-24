0565a1fe72b25aec4505a598b4a922e215986bdb
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-24 23:44:26 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -10,42 +10,73 @@ using namespace std;
 class Solution {
   public:
 	void shortest_distance(vector<vector<int>>&matrix){
-	    int V=matrix.size();
-	    for(int i=0;i<V;i++){
-	        for(int j=0;j<V;j++){
-	            if(matrix[i][j]==-1)
-	            matrix[i][j]=INT_MAX;//Change it to INT_MAX as we are taking i=
-	            //minimum of mat[i][j] and if it's -1 so everytime t may give negative value
-	            
-	        }
-	    }
+	   int V=matrix.size();
+	   
+	   for(int i=0;i<V;i++){
+	       for(int j=0;j<V;j++){
+	           if(matrix[i][j]==-1)
+	           matrix[i][j]=INT_MAX;
+	           }
+	   }
+	   
+	   for(int k=0;k<V;k++){//intermediate
+	       for(int i=0;i<V;i++){
+	           for(int j=0;j<V;j++){
+	               if(matrix[i][k]!=INT_MAX&&matrix[k][j]!=INT_MAX)
+	           {
+	               matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+	           }
+	               
+	           }
+	       }
+	   }
+	   
 	    
-	    for(int k=0;k<V;k++)//Intermediate
-	    {
-	        for(int i=0;i<V;i++)//Source
-	        {
-	            for(int j=0;j<V;j++)//Destination
-	            {       
+	   for(int i=0;i<V;i++){
+	       for(int j=0;j<V;j++){
+	           if(matrix[i][j]==INT_MAX)
+	           matrix[i][j]=-1;
+	           }
+	   }
+	   
+	   
+	}
+	
+// 	 int V=matrix.size();
+// 	    for(int i=0;i<V;i++){
+// 	        for(int j=0;j<V;j++){
+// 	            if(matrix[i][j]==-1)
+// 	            matrix[i][j]=INT_MAX;//Change it to INT_MAX as we are taking i=
+// 	            //minimum of mat[i][j] and if it's -1 so everytime t may give negative value
 	            
-	            if(matrix[i][k]!=INT_MAX&&matrix[k][j]!=INT_MAX)
-	            {
-	            matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);    
-	            }
+// 	        }
+// 	    }
+	    
+// 	    for(int k=0;k<V;k++)//Intermediate
+// 	    {
+// 	        for(int i=0;i<V;i++)//Source
+// 	        {
+// 	            for(int j=0;j<V;j++)//Destination
+// 	            {       
+	            
+// 	            if(matrix[i][k]!=INT_MAX&&matrix[k][j]!=INT_MAX)
+// 	            {
+// 	            matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);    
+// 	            }
 	            
 	                
-	            }
-    	    }
-        }
+// 	            }
+//     	    }
+//         }
 	    
-	    for(int i=0;i<V;i++)
-	    {
-	        for(int j=0;j<V;j++)
-	        {
-	            if(matrix[i][j]==INT_MAX)
-	            matrix[i][j]=-1;
-	        }
-	    }
-	}
+// 	    for(int i=0;i<V;i++)
+// 	    {
+// 	        for(int j=0;j<V;j++)
+// 	        {
+// 	            if(matrix[i][j]==INT_MAX)
+// 	            matrix[i][j]=-1;
+// 	        }
+// 	    }
 };
 
 // { Driver Code Starts.
