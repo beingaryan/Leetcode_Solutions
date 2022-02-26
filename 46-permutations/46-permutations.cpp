290e3b0db703bf62b0433f86a28d161b5e304adf
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-02-27 02:19:36 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (92.01%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,51 +1,116 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        int n=nums.size();      
-        vector<int>visited(n+1,0);
         
-        vector<vector<int>>ans;
-        vector<int>vec;
+        int n=nums.size();
+        vector<int>visited(n,0);
         
-//            for(int i=0;i<visited.size();i++)
-//             {
-//                 cout<<visited[i]<<" ";
-//             }
-//             cout<<endl;
+        vector<vector<int>>vec;
+        vector<int>temp;
         
-        permuteWays(nums,visited,vec,ans);
-        return ans;
+        fun(vec,temp,visited,n,nums);
+        return vec;
     }
     
-    void permuteWays(vector<int> &nums,vector<int> &visited,vector<int> &vec,vector<vector<int>> &ans){
-        //BC
-        if(vec.size()==nums.size())
-        {
-            // for(int i=0;i<vec.size();i++)
-            // {
-            //     cout<<vec[i]<<" ";
-            // }
-            // cout<<endl;
-            ans.push_back(vec);
+    void fun(vector<vector<int>>&vec,vector<int>&temp,vector<int>&visited,int n,vector<int>& nums)
+    {
+        if(temp.size()==n){
+            vec.push_back(temp);
             return;
         }
-        
-        
-        for(int i=0;i<nums.size();i++)
-        {
-            if(visited[i]==0)
-            {
-                
-                vec.push_back(nums[i]);
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
                 visited[i]=1;
-                permuteWays(nums,visited,vec,ans);
+                temp.push_back(nums[i]);
+                fun(vec,temp,visited,n,nums);
                 visited[i]=0;
-                vec.pop_back();
+                temp.pop_back();
             }
-            
-            // 
         }
+        
         return;
+        
+     
     }
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     int n=nums.size();      
+//         vector<int>visited(n+1,0);
+        
+//         vector<vector<int>>ans;
+//         vector<int>vec;
+        
+// //            for(int i=0;i<visited.size();i++)
+// //             {
+// //                 cout<<visited[i]<<" ";
+// //             }
+// //             cout<<endl;
+        
+//         permuteWays(nums,visited,vec,ans);
+//         return ans;
+//     }
+    
+//     void permuteWays(vector<int> &nums,vector<int> &visited,vector<int> &vec,vector<vector<int>> &ans){
+//         //BC
+//         if(vec.size()==nums.size())
+//         {
+//             // for(int i=0;i<vec.size();i++)
+//             // {
+//             //     cout<<vec[i]<<" ";
+//             // }
+//             // cout<<endl;
+//             ans.push_back(vec);
+//             return;
+//         }
+        
+        
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(visited[i]==0)
+//             {
+                
+//                 vec.push_back(nums[i]);
+//                 visited[i]=1;
+//                 permuteWays(nums,visited,vec,ans);
+//                 visited[i]=0;
+//                 vec.pop_back();
+//             }
+            
+//             // 
+//         }
+//         return;
 };
