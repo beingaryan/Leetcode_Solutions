3b550f7415800a0e9445816e0a01686318ba4162
Aryan Gupta <65234088+beingaryan@users.noreply.github.com>
2022-04-20 16:04:48 +0530

    Added solution - LeetHub

---
+++ Minimum element in a sorted and rotated array - GFG/minimum-element-in-a-sorted-and-rotated-array.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+    int findMin(int arr[], int n){
+        int low=0;
+        int high=n-1;
+        int mid=-1;
+        // if(arr[0]<=arr[n-1])
+        // return arr[0];
+        while(low<=high){
+           
+            mid=(high+low)/2;
+            int prev=(n+mid-1)%n;
+            int next=(mid+1)%n;
+            
+            if(arr[mid]<=arr[prev]&&arr[mid]<=arr[next])
+                return arr[mid];
+                
+            else if(arr[mid]>=arr[high])
+            low=mid+1;
+            else
+            high=mid-1;
+        }
+        
+        return arr[mid];
+    }
+
+};
+
+// { Driver Code Starts.
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution ob;
+		cout<<ob.findMin(a, n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
